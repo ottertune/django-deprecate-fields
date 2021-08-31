193b46a2cc9f2773c6614a8841b2313df596f5f7
Neal Kaviratna <kaviratnaneal@gmail.com>
2021-08-31 16:48:43 -0400

    switch to patching original field instance

--- django_deprecate_fields/deprecate_field.py
+++ django_deprecate_fields/deprecate_field.py
@@ -4,45 +4,42 @@ import warnings
 
 logger = logging.getLogger(__file__)
 
-
-class DeprecatedField(object):
+def __deprecate_warn_get_name(self, obj):
     """
-    Descriptor class for deprecated fields. Do not use directly, use the
-    deprecate_field function instead.
+    Try to find this field's name in the model class
     """
+    for k, v in type(obj).__dict__.items():
+        if v is self:
+            return k
+    return "<unknown>"
 
-    def __init__(self, val):
-        self.val = val
+def __deprecate_warn_and_return_substitute__get__(self, obj, type=None):
+    msg = "accessing deprecated field %s.%s" % (
+        obj.__class__.__name__,
+        self._get_name(obj),
+    )
+    warnings.warn(msg, DeprecationWarning, stacklevel=2)
+    logger.warning(msg)
+    if not callable(self.__patched_return_value):
+        return self.__patched_return_value
+    return self.__patched_return_value()
 
-    def _get_name(self, obj):
-        """
-        Try to find this field's name in the model class
-        """
-        for k, v in type(obj).__dict__.items():
-            if v is self:
-                return k
-        return "<unknown>"
-
-    def __get__(self, obj, type=None):
-        msg = "accessing deprecated field %s.%s" % (
-            obj.__class__.__name__,
-            self._get_name(obj),
-        )
-        warnings.warn(msg, DeprecationWarning, stacklevel=2)
-        logger.warning(msg)
-        if not callable(self.val):
-            return self.val
-        return self.val()
-
-    def __set__(self, obj, val):
-        msg = "writing to deprecated field %s.%s" % (
-            obj.__class__.__name__,
-            self._get_name(obj),
-        )
-        warnings.warn(msg, DeprecationWarning, stacklevel=2)
-        logger.warning(msg)
-        self.val = val
-
+def __deprecate_warn__set__(self, obj, val):
+    msg = "writing to deprecated field %s.%s" % (
+        obj.__class__.__name__,
+        self._get_name(obj),
+    )
+    warnings.warn(msg, DeprecationWarning, stacklevel=2)
+    logger.warning(msg)
+    self.__original_set(val)
+    
+def monkey_patch_deprecated_methods(field_instance, return_replacement):
+    field_instance.__patched_return_value = return_replacement
+    field_instance._get_name = __deprecate_warn_get_name
+    field_instance.__get__ = __deprecate_warn_and_return_substitute__get__
+    field_instance.__original_set = field.instance.__set__
+    field_instance.__set__ = __deprecate_warn__set__
+    return field_instance
 
 def deprecate_field(field_instance, return_instead=None):
     """
@@ -57,8 +54,8 @@ def deprecate_field(field_instance, return_instead=None):
     :param return_instead: A value or function that
     the field will pretend to have
     """
-    if not set(sys.argv) & {"makemigrations", "migrate", "showmigrations"}:
-        return DeprecatedField(return_instead)
-
     field_instance.null = True
+    if not set(sys.argv) & {"makemigrations", "migrate", "showmigrations"}:
+        monkey_patch_deprecated_methods(field_instance)
+
     return field_instance
